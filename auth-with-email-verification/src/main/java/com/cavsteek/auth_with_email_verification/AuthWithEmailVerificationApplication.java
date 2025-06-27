44e889c2db0a8d8af776b664c0928488479db0b9
Uzochukwu Ikemdinachi <uzochukwuikemdinachi@gmail.com>
2025-06-27 23:57:41 +0100

    first commit

---
+++ auth-with-email-verification/src/main/java/com/cavsteek/auth_with_email_verification/AuthWithEmailVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.cavsteek.auth_with_email_verification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthWithEmailVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthWithEmailVerificationApplication.class, args);
+	}
+
+}
---
+++ auth-with-email-verification/src/test/java/com/cavsteek/auth_with_email_verification/AuthWithEmailVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cavsteek.auth_with_email_verification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthWithEmailVerificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
