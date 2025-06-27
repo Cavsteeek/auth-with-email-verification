489f045c481730b8e1715fe75204af800cd34b89
Uzochukwu Ikemdinachi <uzochukwuikemdinachi@gmail.com>
2025-06-28 00:55:55 +0100

    created and setup UserRepository

---
+++ auth-with-email-verification/src/main/java/com/cavsteek/auth_with_email_verification/repository/UserRepo.java
@@ -0,0 +1,15 @@
+package com.cavsteek.auth_with_email_verification.repository;
+
+import com.cavsteek.auth_with_email_verification.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.OptionalInt;
+
+@Repository
+public interface UserRepo extends CrudRepository<User, Long> {
+    Optional<User> findByEmail (String email);
+    Optional<User> findByVerificationCode(String verificationCode);
+}
